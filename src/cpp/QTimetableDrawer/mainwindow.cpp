fbf1c547ba6346b56439b5f114f9a61bcb0b9b90
Gregor Heiming <gregor.heiming@rwth-aachen.de>
2016-08-12 14:22:55 +0200

    Check if all input are made for timetable creation
    Rename ui widgets

--- src/cpp/QTimetableDrawer/mainwindow.cpp
+++ src/cpp/QTimetableDrawer/mainwindow.cpp
@@ -1,12 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "qfiledialog.h"
+#include <string>
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
-  ui->tableWidget->setColumnWidth(0,200);
+  ui->colorTable->setColumnWidth(0,200);
 }
 
 MainWindow::~MainWindow()
@@ -14,14 +17,106 @@ MainWindow::~MainWindow()
   delete ui;
 }
 
-void MainWindow::on_checkBox_toggled(bool checked)
+bool MainWindow::readyForTimetableCreation()
 {
-  if(checked)
+  if(ui->inputFileLineEdit->text() != ""
+     && ui->outputFileLineEdit->text() != "")
   {
-    ui->tableWidget->setEnabled(true);
+    if(ui->colorCheckBox->isChecked())
+    {
+      QTableWidgetItem* item = ui->colorTable->item(0,0);
+      if(!item || item->text().isEmpty())
+      {
+        return false;
+      }
+    }
   }
   else
   {
-    ui->tableWidget->setEnabled(false);
+    return false;
+  }
+  return true;
+}
+
+void MainWindow::enableCreationButtons()
+{
+  ui->createButton->setEnabled(true);
+  ui->actionCreateTimetable->setEnabled(true);
+  ui->statusBar->showMessage("Bereit zum Erstellen des Stundenplans");
+}
+
+void MainWindow::disableCreationButtons()
+{
+  ui->createButton->setEnabled(false);
+  ui->actionCreateTimetable->setEnabled(false);
+  ui->statusBar->clearMessage();
+}
+
+void MainWindow::on_colorCheckBox_toggled(bool checked)
+{
+  if(checked)
+  {
+    ui->colorTable->setEnabled(true);
+  }
+  else
+  {
+    ui->colorTable->setEnabled(false);
+  }
+
+  if(readyForTimetableCreation())
+  {
+    enableCreationButtons();
+  }
+  else
+  {
+    disableCreationButtons();
+  }
+}
+
+void MainWindow::on_inputFileSelectionButton_clicked()
+{
+  QString inputFileName = QFileDialog::getOpenFileName(this,tr("Eingabedatei auswählen"),"",tr("CSV Datei (*.csv)"));
+  ui->inputFileLineEdit->setText(inputFileName);
+}
+
+void MainWindow::on_outputFileSelectionButton_clicked()
+{
+  QString outputFileName = QFileDialog::getSaveFileName(this,tr("Ausgabedatei auswählen"),"",tr("PDF Datei (*.pdf)"));
+  ui->outputFileLineEdit->setText(outputFileName);
+}
+
+void MainWindow::on_inputFileLineEdit_textChanged(const QString &arg1)
+{
+  if(readyForTimetableCreation())
+  {
+    enableCreationButtons();
+  }
+  else
+  {
+    disableCreationButtons();
+  }
+}
+
+void MainWindow::on_outputFileLineEdit_textChanged(const QString &arg1)
+{
+  if(readyForTimetableCreation())
+  {
+    enableCreationButtons();
+  }
+  else
+  {
+    disableCreationButtons();
+  }
+}
+
+void MainWindow::on_colorTable_cellChanged(int row, int column)
+{
+  if(readyForTimetableCreation())
+  {
+    enableCreationButtons();
+  }
+  else
+  {
+    disableCreationButtons();
   }
 }
--- src/cpp/QTimetableDrawer/mainwindow.h
+++ src/cpp/QTimetableDrawer/mainwindow.h
@@ -11,15 +11,28 @@ class MainWindow : public QMainWindow
 {
   Q_OBJECT
 
-  public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+public:
+  explicit MainWindow(QWidget *parent = 0);
+  ~MainWindow();
 
-    private slots:
-      void on_checkBox_toggled(bool checked);
+private slots:
+  void on_colorCheckBox_toggled(bool checked);
 
-  private:
+  void on_inputFileSelectionButton_clicked();
+
+  void on_outputFileSelectionButton_clicked();
+
+  void on_inputFileLineEdit_textChanged(const QString &arg1);
+
+  void on_outputFileLineEdit_textChanged(const QString &arg1);
+
+  void on_colorTable_cellChanged(int row, int column);
+
+private:
     Ui::MainWindow *ui;
+    bool readyForTimetableCreation();
+    void enableCreationButtons();
+    void disableCreationButtons();
 };
 
 #endif // MAINWINDOW_H
